4edc6f56ff0c46ccaf00e6811840998a2b7af8f6
Stefano Teodorani <teopost@gmail.com>
2023-09-12 23:41:45 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,6 @@
 sudo apt update -y
 sudo apt full-upgrade -y
 
-
 # install docker
 # https://docs.docker.com/engine/install/raspberry-pi-os/
 # --------------
@@ -9,7 +8,7 @@ cd ~
 for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
 sudo apt-get update -y
-sudo apt-get install ca-certificates curl gnupg
+sudo apt-get install ca-certificates curl gnupg mc
 
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/raspbian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -24,45 +23,33 @@ sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 
-
-# install naspi-bootstrap scripts
-# --------------------------------
+# install naspi scripts
+# ---------------------
+# https://wiki.geekworm.com/XScript
 cd ~
-git clone https://github.com/teopost/boxpi-bootstrap.git
-cd boxpi-bootstrap/systemd/
-mkdir /data
-sudo cp ~/boxpi-bootstrap/systemd/data.mount            /etc/systemd/system/
-sudo cp ~/boxpi-bootstrap/systemd/reset-usb.service   /etc/systemd/system/
+sudo echo "dtoverlay=pwm-2chan" >> "/boot/config.txt"
+git clone https://github.com/geekworm-com/xscript
+cd xscript
+chmod +x *.sh
+
+# install the x-c1-fan service
+sudo cp -f ./x-c1-fan.sh                /usr/local/bin/
+sudo cp -f ./x-c1-fan.service           /lib/systemd/system
 sudo systemctl daemon-reload
-sudo systemctl enable data.mount
-sudo systemctl enable reset-usb.service
-sudo systemctl start data.mount
-sudo systemctl start reset-usb.service
+sudo systemctl enable x-c1-fan
+sudo systemctl start x-c1-fan
 
+# install the x-c1-pwr service
+sudo cp -f ./x-c1-pwr.sh                /usr/local/bin/
+sudo cp -f x-c1-pwr.service             /lib/systemd/system
+sudo systemctl daemon-reload
+sudo systemctl enable x-c1-pwr
+sudo systemctl start x-c1-pwr
 
-/usr/sbin/uhubctl -a 2 -l 2
+# Prepair software shutdown script
+sudo cp -f ./x-c1-softsd.sh             /usr/local/bin/
 
+# create alis for software shutdown
+sudo echo "alias xoff='sudo /usr/local/bin/x-c1-softsd.sh'" > /etc/profile.d/custom-aliases.sh
+sudo chmod 777 /etc/profile.d/custom-aliases.sh
 
-# install deskpi scripts
-# ----------------------
-cd ~
-git clone https://github.com/DeskPi-Team/deskpi.git
-cd ./deskpi/
-chmod +x install.sh
-sudo ./install.sh
-cd ..
-
-# the system reboot now
-
-exit
-
-# install uhubctl (optional)
-# ---------------
-cd ~
-sudo apt install -y libusb-1.0-0-dev
-git clone https://github.com/mvp/uhubctl 
-cd uhubctl 
-make
-sudo make install
-/usr/sbin/uhubctl -a 2 -l 2
-lsblk 
