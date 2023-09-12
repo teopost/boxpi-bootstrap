07c111769f0d469af984852cecdf8ce080fea0c1
Stefano Teodorani <teopost@gmail.com>
2023-09-12 23:32:59 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,5 @@
-sudo apt update
+sudo apt update -y
 sudo apt full-upgrade -y
-sudo apt install -y git
-
-
-# install uhubctl
-# ---------------
-cd ~
-sudo apt install -y libusb-1.0-0-dev
-git clone https://github.com/mvp/uhubctl 
-cd uhubctl 
-make
-sudo make install
-/usr/sbin/uhubctl -a 2 -l 2
-lsblk 
-
 
 
 # install docker
@@ -22,7 +8,7 @@ lsblk
 cd ~
 for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
-sudo apt-get update 
+sudo apt-get update -y
 sudo apt-get install ca-certificates curl gnupg
 
 sudo install -m 0755 -d /etc/apt/keyrings
@@ -39,7 +25,7 @@ sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plug
 
 
 
-# install boxpi-bootstrap scripts
+# install naspi-bootstrap scripts
 # --------------------------------
 cd ~
 git clone https://github.com/teopost/boxpi-bootstrap.git
@@ -68,33 +54,15 @@ cd ..
 
 # the system reboot now
 
-
-
-# --------------------------------------------------
-
-
-
 exit
 
-
-
-
-# configure github key
-ssh-keygen
-cat /root/.ssh/id_rsa.pub
-# add key to github
-git clone -y git@github.com:teopost/boxpi-bootstrap.git /home/teopost/boxpi-bootstrap
-cd /home/teopost/boxpi-bootstrap/systemd/
-mkdir /data
-
-
-
-
-dtoverlay=disable-wifi
-dtoverlay=disable-bt
-
-
-# relocate docker dir
-https://www.ibm.com/docs/en/z-logdata-analytics/5.1.0?topic=compose-relocating-docker-root-directory
-
-174c:55aa
+# install uhubctl (optional)
+# ---------------
+cd ~
+sudo apt install -y libusb-1.0-0-dev
+git clone https://github.com/mvp/uhubctl 
+cd uhubctl 
+make
+sudo make install
+/usr/sbin/uhubctl -a 2 -l 2
+lsblk 
